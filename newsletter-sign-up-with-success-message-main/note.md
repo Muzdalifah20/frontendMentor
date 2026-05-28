dd0d45301d8d24dd470cf041354165c2073ec9f2
muzdalifah <muzdalifahsuliman@gmail.com>
2026-05-28 14:28:49 +0300

    complete sign-up letter challange

---
+++ newsletter-sign-up-with-success-message-main/note.md
@@ -0,0 +1,13 @@
+<!-- techniques before for when you want a full-width element within a constrained-width column -->
+
+https://css-tricks.com/full-width-containers-limited-width-parents/
+
+https://www.cyishere.dev/blog/full-bleed-within-a-constrained-wrapper
+
+<!-- gradient background -->
+
+https://developer.mozilla.org/en-US/docs/Web/CSS/Guides/Images/Using_gradients
+
+<!-- caret -->
+
+https://developer.mozilla.org/en-US/docs/Web/CSS/Reference/Properties/caret
---
+++ newsletter-sign-up-with-success-message-main/script.js
@@ -0,0 +1,54 @@
+const form = document.querySelector(".form");
+const email = document.getElementById("email");
+const subscribeBtn = document.querySelector("#subscribeBtn");
+const signup = document.querySelector(".signup");
+const success = document.querySelector(".success");
+const inValidP = document.querySelector("#inValid");
+const dismissmessageBtn = document.querySelector("#dismissmessageBtn");
+// console.log(emailInput);
+
+form.addEventListener("submit", (e) => {
+  e.preventDefault();
+
+  const emailValue = email.value.trim();
+
+  let isvalid = true;
+
+  const pattren = /^[a-z0-9._+-]+@[a-z0-9.-]+\.[a-z]{2,}$/;
+  if (!emailValue) {
+    isvalid = false;
+    inValidP.textContent = "";
+    inValidP.textContent = "Email can't be empty!";
+    inValidP.style.color = "var(--color-invalid)";
+    inputStyle();
+  } else if (!pattren.test(emailValue)) {
+    isvalid = false;
+    inValidP.textContent = "";
+    inValidP.textContent = "Valid email required!";
+    inValidP.style.color = "var(--color-invalid)";
+    inputStyle();
+  } else {
+    inValidP.textContent = "";
+    email.value = "";
+    successStyle();
+  }
+
+  function inputStyle() {
+    email.classList.add("in-valid");
+  }
+
+  function successStyle() {
+    signup.style.display = "none";
+    success.style.display = "block";
+  }
+});
+
+email.addEventListener("input", () => {
+  email.classList.remove("in-valid");
+});
+
+dismissmessageBtn.addEventListener("click", (e) => {
+  e.preventDefault();
+  signup.style.display = "block";
+  success.style.display = "none";
+});
