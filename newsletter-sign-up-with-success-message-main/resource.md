7f66a07c16fa05ba4c7f9ac95589e1dd9f6663d3
muzdalifah <muzdalifahsuliman@gmail.com>
2026-05-29 08:12:57 +0300

    refractor code

--- newsletter-sign-up-with-success-message-main/resource.md
+++ newsletter-sign-up-with-success-message-main/resource.md
@@ -1,13 +1,17 @@
-<!-- techniques before for when you want a full-width element within a constrained-width column -->
+# Project Resources
+
+List of resources and links I used while building this project:
+
+- techniques before for when you want a full-width element within a constrained-width column
 
 https://css-tricks.com/full-width-containers-limited-width-parents/
 
 https://www.cyishere.dev/blog/full-bleed-within-a-constrained-wrapper
 
-<!-- gradient background -->
+- gradient background
 
 https://developer.mozilla.org/en-US/docs/Web/CSS/Guides/Images/Using_gradients
 
-<!-- caret -->
+- caret
 
 https://developer.mozilla.org/en-US/docs/Web/CSS/Reference/Properties/caret
--- newsletter-sign-up-with-success-message-main/script.js
+++ newsletter-sign-up-with-success-message-main/script.js
@@ -4,37 +4,42 @@ const subscribeBtn = document.querySelector("#subscribeBtn");
 const signup = document.querySelector(".signup");
 const success = document.querySelector(".success");
 const inValidP = document.querySelector("#inValid");
+const successMessage = document.querySelector("#successMessage");
 const dismissmessageBtn = document.querySelector("#dismissmessageBtn");
-// console.log(emailInput);
 
 form.addEventListener("submit", (e) => {
   e.preventDefault();
 
   const emailValue = email.value.trim();
+  clearError();
+  const emailPattren = /^[a-z0-9._%+-]+@[a-z0-9.-]+\.[a-z]{2,}$/;
 
   let isvalid = true;
-
-  const pattren = /^[a-z0-9._%+-]+@[a-z0-9.-]+\.[a-z]{2,}$/;
   if (!emailValue) {
     isvalid = false;
-    inValidP.textContent = "";
-    inValidP.textContent = "Email can't be empty!";
-    inValidP.style.color = "var(--color-invalid)";
-    inputStyle();
-  } else if (!pattren.test(emailValue)) {
+
+    showError("Email can't be empty!");
+  } else if (!emailPattren.test(emailValue)) {
     isvalid = false;
-    inValidP.textContent = "";
-    inValidP.textContent = "Valid email required!";
-    inValidP.style.color = "var(--color-invalid)";
-    inputStyle();
+
+    showError("Valid email required!");
   } else {
-    inValidP.textContent = "";
+    successMessage.textContent = `A confirmation email has been sent to ${emailValue}. Please open it and click the button inside to confirm your subscription.`;
+
     email.value = "";
     successStyle();
   }
 
-  function inputStyle() {
+  function showError(message) {
+    inValidP.textContent = message;
+    inValidP.classList.add("in-valid-message");
     email.classList.add("in-valid");
+  }
+
+  function clearError() {
+    inValidP.textContent = "";
+    inValidP.classList.remove("in-valid-message");
+    email.classList.remove("in-valid");
   }
 
   function successStyle() {
