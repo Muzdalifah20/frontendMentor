44c18cb864ce8c2422b71f69750ab8d7f1ade006
Muzdalifah <muzdalifahsuliman@gmail.com>
2026-07-01 07:56:30 +0300

    Refractor Js, add hidden class and add aria* for accessiblity

--- newsletter-sign-up-with-success-message-main/script.js
+++ newsletter-sign-up-with-success-message-main/script.js
@@ -1,60 +1,65 @@
 const form = document.querySelector(".form");
-const email = document.getElementById("email");
+const emailInput = document.getElementById("email");
 const subscribeBtn = document.querySelector("#subscribeBtn");
 const signup = document.querySelector(".signup");
 const success = document.querySelector(".success");
-const inValidP = document.querySelector("#inValid");
+const emailErrorP = document.querySelector("#emailError");
 const successMessage = document.querySelector("#successMessage");
 const dismissmessageBtn = document.querySelector("#dismissmessageBtn");
 
 form.addEventListener("submit", (e) => {
   e.preventDefault();
 
-  const emailValue = email.value.trim();
+  const emailValue = emailInput.value.trim();
   clearError();
   const emailPattren = /^[a-z0-9._%+-]+@[a-z0-9.-]+\.[a-z]{2,}$/;
 
-  let isvalid = true;
+  let isvalidEmail = true;
   if (!emailValue) {
-    isvalid = false;
-
+    isvalidEmail = false;
+    emailInput.setAttribute("aria-invalid", "true");
     showError("Email can't be empty!");
   } else if (!emailPattren.test(emailValue)) {
-    isvalid = false;
-
+    isvalidEmail = false;
+    emailInput.setAttribute("aria-invalid", "true");
     showError("Valid email required!");
   } else {
+    emailInput.setAttribute("aria-invalid", "false");
     successMessage.textContent = `A confirmation email has been sent to ${emailValue}. Please open it and click the button inside to confirm your subscription.`;
 
-    email.value = "";
-    successStyle();
+    emailInput.value = "";
+    hideSection(signup);
+    ShowSection(success);
   }
 });
 
 function showError(message) {
-  inValidP.textContent = message;
-  inValidP.classList.add("in-valid-message");
-  email.classList.add("in-valid");
+  emailErrorP.textContent = message;
+  emailErrorP.classList.add("in-valid-message");
+  emailInput.classList.add("in-valid");
 }
 
 function clearError() {
-  inValidP.textContent = "";
-  inValidP.classList.remove("in-valid-message");
-  email.classList.remove("in-valid");
+  emailErrorP.textContent = "";
+  emailErrorP.classList.remove("in-valid-message");
+  emailInput.classList.remove("in-valid");
 }
 
-function successStyle() {
-  signup.style.display = "none";
-  success.style.display = "block";
+function ShowSection(section) {
+  section.classList.remove("hidden");
 }
 
-email.addEventListener("input", () => {
-  email.classList.remove("in-valid");
+function hideSection(section) {
+  section.classList.add("hidden");
+}
+
+emailInput.addEventListener("input", () => {
+  emailInput.classList.remove("in-valid");
   clearError();
 });
 
 dismissmessageBtn.addEventListener("click", (e) => {
   e.preventDefault();
-  signup.style.display = "block";
-  success.style.display = "none";
+  ShowSection(signup);
+  hideSection(success);
 });
